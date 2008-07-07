a19530d47860eb7a0c8b1de4310ac9ad859287b6
robertgonda <robertgonda@735d13b6-9817-0410-8766-e36946ffe9aa>
2008-07-07 12:31:13 +0000

    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/rtg_files/trunk@10412 735d13b6-9817-0410-8766-e36946ffe9aa

---
+++ ext_tables.sql
@@ -0,0 +1,44 @@
+#
+# Table structure for table 'tx_rtgfiles_files'
+#
+CREATE TABLE tx_rtgfiles_files (
+	uid int(11) NOT NULL auto_increment,
+	pid int(11) DEFAULT '0' NOT NULL,
+	tstamp int(11) DEFAULT '0' NOT NULL,
+	crdate int(11) DEFAULT '0' NOT NULL,
+	cruser_id int(11) DEFAULT '0' NOT NULL,
+	sorting int(10) DEFAULT '0' NOT NULL,
+	deleted tinyint(4) DEFAULT '0' NOT NULL,
+	hidden tinyint(4) DEFAULT '0' NOT NULL,
+	title varchar(255) DEFAULT '' NOT NULL,
+	system int(11) DEFAULT '0' NOT NULL,
+	description text NOT NULL,
+	keywords text NOT NULL,
+	file blob NOT NULL,
+	url varchar(255) DEFAULT '' NOT NULL,
+	clicks int(11) DEFAULT '0' NOT NULL,
+	
+	PRIMARY KEY (uid),
+	KEY parent (pid)
+);
+
+
+
+#
+# Table structure for table 'tx_rtgfiles_systems'
+#
+CREATE TABLE tx_rtgfiles_systems (
+	uid int(11) NOT NULL auto_increment,
+	pid int(11) DEFAULT '0' NOT NULL,
+	tstamp int(11) DEFAULT '0' NOT NULL,
+	crdate int(11) DEFAULT '0' NOT NULL,
+	cruser_id int(11) DEFAULT '0' NOT NULL,
+	sorting int(10) DEFAULT '0' NOT NULL,
+	deleted tinyint(4) DEFAULT '0' NOT NULL,
+	hidden tinyint(4) DEFAULT '0' NOT NULL,
+	title varchar(80) DEFAULT '' NOT NULL,
+	description text NOT NULL,
+	
+	PRIMARY KEY (uid),
+	KEY parent (pid)
+);
